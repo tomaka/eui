d85b8b7ebc35eb52dc3d970731b9ec8893d2da6d
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-09-28 09:25:47 +0200

    Cleanup ImageButton

--- src/predefined/image_button.rs
+++ src/predefined/image_button.rs
@@ -8,6 +8,7 @@ use Widget;
 use predefined::Image;
 use predefined::{MouseEnterEvent, MouseLeaveEvent};
 
+/// An image that can be used as a button. Has a regular image and a hovered image.
 pub struct ImageButton {
     hovered: bool,
     image_normal: Image,
@@ -37,45 +38,13 @@ impl Widget for ImageButton {
             self.image_normal.build_layout(height_per_width, alignment)
         }
     }
-}
 
-/*impl Widget for Button {
     #[inline]
-    fn draw(&self) -> Vec<Shape> {
+    fn needs_rebuild(&self) -> bool {
         if self.hovered {
-            self.image_hovered.draw()
+            self.image_hovered.needs_rebuild()
         } else {
-            self.image_normal.draw()
+            self.image_normal.needs_rebuild()
         }
     }
-
-    #[inline]
-    fn set_dimensions(&mut self, matrix: &Matrix, viewport_height_per_width: f32)
-                      -> Vec<Box<Event>>
-    {
-        // TODO: propagate events
-
-        self.image_normal.set_dimensions(matrix, viewport_height_per_width);
-        self.image_hovered.set_dimensions(matrix, viewport_height_per_width);
-
-        vec![]
-    }
-
-    #[inline]
-    fn set_cursor(&mut self, cursor: Option<[f32; 2]>) -> Vec<Box<Event>> {
-        let hovered = match cursor {
-            Some(pos) => pos[0] >= -1.0 && pos[0] <= 1.0 && pos[1] >= -1.0 && pos[1] <= 1.0,
-            None => false,
-        };
-
-        let events = match (self.hovered, hovered) {
-            (false, true) => vec![Box::new(MouseEnterEvent) as Box<Event>],
-            (true, false) => vec![Box::new(MouseLeaveEvent) as Box<Event>],
-            _ => vec![]
-        };
-
-        self.hovered = hovered;
-
-        events
-    }
-}*/
+}

74c13659f05d6b8569eab1c3bbdac2039a724833
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-09-28 12:09:37 +0200

    Improve Shape::hit_test docs and implementation

--- src/lib.rs
+++ src/lib.rs
@@ -411,49 +411,66 @@ impl Shape {
 
     /// Returns true if the point's coordinates hit the shape.
     pub fn hit_test(&self, point: &[f32; 2]) -> bool {
+        /// Calculates whether the point is in a rectangle multiplied by a matrix.
+        fn test(matrix: &Matrix, point: &[f32; 2]) -> bool {
+            // We start by calculating the positions of the four corners of the shape in viewport
+            // coordinates, so that they can be compared with the point which is already in
+            // viewport coordinates.
+
+            let top_left = *matrix * [-1.0, 1.0, 1.0];
+            let top_left = [top_left[0] / top_left[2], top_left[1] / top_left[2]];
+
+            let top_right = *matrix * [1.0, 1.0, 1.0];
+            let top_right = [top_right[0] / top_right[2], top_right[1] / top_right[2]];
+
+            let bot_left = *matrix * [-1.0, -1.0, 1.0];
+            let bot_left = [bot_left[0] / bot_left[2], bot_left[1] / bot_left[2]];
+
+            let bot_right = *matrix * [1.0, -1.0, 1.0];
+            let bot_right = [bot_right[0] / bot_right[2], bot_right[1] / bot_right[2]];
+
+            // The point is within our rectangle if and only if it is on the right side of each
+            // border of the rectangle (taken in the right order).
+            //
+            // To check this, we calculate the dot product of the vector `point - corner` with
+            // `next_corner - corner`. If the value is positive, then the angle is inferior to
+            // 90°. If the the value is negative, the angle is superior to 90° and we know that
+            // the cursor is outside of the rectangle.
+
+            if (point[0] - top_left[0]) * (top_right[0] - top_left[0]) +
+               (point[1] - top_left[1]) * (top_right[1] - top_left[1]) < 0.0
+            {
+                return false;
+            }
+
+            if (point[0] - top_right[0]) * (bot_right[0] - top_right[0]) +
+               (point[1] - top_right[1]) * (bot_right[1] - top_right[1]) < 0.0
+            {
+                return false;
+            }
+
+            if (point[0] - bot_right[0]) * (bot_left[0] - bot_right[0]) +
+               (point[1] - bot_right[1]) * (bot_left[1] - bot_right[1]) < 0.0
+            {
+                return false;
+            }
+
+            if (point[0] - bot_left[0]) * (top_left[0] - bot_left[0]) +
+               (point[1] - bot_left[1]) * (top_left[1] - bot_left[1]) < 0.0
+            {
+                return false;
+            }
+
+            true
+        }
+
         match self {
-            &Shape::Text { .. } => {
-                false       // FIXME: 
+            &Shape::Text { ref matrix, .. } => {
+                test(matrix, point)
             },
 
             &Shape::Image { ref matrix, .. } => {
-                let top_left = *matrix * [-1.0, 1.0, 1.0];
-                let top_left = [top_left[0] / top_left[2], top_left[1] / top_left[2]];
-
-                let top_right = *matrix * [1.0, 1.0, 1.0];
-                let top_right = [top_right[0] / top_right[2], top_right[1] / top_right[2]];
-
-                let bot_left = *matrix * [-1.0, -1.0, 1.0];
-                let bot_left = [bot_left[0] / bot_left[2], bot_left[1] / bot_left[2]];
-
-                let bot_right = *matrix * [1.0, -1.0, 1.0];
-                let bot_right = [bot_right[0] / bot_right[2], bot_right[1] / bot_right[2]];
-
-                if (point[0] - top_left[0]) * (top_right[0] - top_left[0]) +
-                   (point[1] - top_left[1]) * (top_right[1] - top_left[1]) < 0.0
-                {
-                    return false;
-                }
-
-                if (point[0] - top_right[0]) * (bot_right[0] - top_right[0]) +
-                   (point[1] - top_right[1]) * (bot_right[1] - top_right[1]) < 0.0
-                {
-                    return false;
-                }
-
-                if (point[0] - bot_right[0]) * (bot_left[0] - bot_right[0]) +
-                   (point[1] - bot_right[1]) * (bot_left[1] - bot_right[1]) < 0.0
-                {
-                    return false;
-                }
-
-                if (point[0] - bot_left[0]) * (top_left[0] - bot_left[0]) +
-                   (point[1] - bot_left[1]) * (top_left[1] - bot_left[1]) < 0.0
-                {
-                    return false;
-                }
-
-                true
+                test(matrix, point)
             },
         }
     }

2a21d23e16fc421446b3f22081e2fb7f20dc4a8a
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-09-28 12:55:44 +0200

    Extract Shape to its own module

---
+++ src/shape.rs
@@ -0,0 +1,90 @@
+use Matrix;
+
+/// A shape that can be drawn by any of the UI's components.
+#[derive(Clone, Debug)]
+pub enum Shape {
+    Text {
+        matrix: Matrix,
+        text: String,
+    },
+    Image {
+        matrix: Matrix,
+        name: String,
+    },
+}
+
+impl Shape {
+    #[inline]
+    pub fn apply_matrix(self, outer: &Matrix) -> Shape {
+        match self {
+            Shape::Text { matrix, text } => Shape::Text { matrix: *outer * matrix, text: text },
+            Shape::Image { matrix, name } => Shape::Image { matrix: *outer * matrix, name: name },
+        }
+    }
+
+    /// Returns true if the point's coordinates hit the shape.
+    pub fn hit_test(&self, point: &[f32; 2]) -> bool {
+        /// Calculates whether the point is in a rectangle multiplied by a matrix.
+        fn test(matrix: &Matrix, point: &[f32; 2]) -> bool {
+            // We start by calculating the positions of the four corners of the shape in viewport
+            // coordinates, so that they can be compared with the point which is already in
+            // viewport coordinates.
+
+            let top_left = *matrix * [-1.0, 1.0, 1.0];
+            let top_left = [top_left[0] / top_left[2], top_left[1] / top_left[2]];
+
+            let top_right = *matrix * [1.0, 1.0, 1.0];
+            let top_right = [top_right[0] / top_right[2], top_right[1] / top_right[2]];
+
+            let bot_left = *matrix * [-1.0, -1.0, 1.0];
+            let bot_left = [bot_left[0] / bot_left[2], bot_left[1] / bot_left[2]];
+
+            let bot_right = *matrix * [1.0, -1.0, 1.0];
+            let bot_right = [bot_right[0] / bot_right[2], bot_right[1] / bot_right[2]];
+
+            // The point is within our rectangle if and only if it is on the right side of each
+            // border of the rectangle (taken in the right order).
+            //
+            // To check this, we calculate the dot product of the vector `point - corner` with
+            // `next_corner - corner`. If the value is positive, then the angle is inferior to
+            // 90°. If the the value is negative, the angle is superior to 90° and we know that
+            // the cursor is outside of the rectangle.
+
+            if (point[0] - top_left[0]) * (top_right[0] - top_left[0]) +
+               (point[1] - top_left[1]) * (top_right[1] - top_left[1]) < 0.0
+            {
+                return false;
+            }
+
+            if (point[0] - top_right[0]) * (bot_right[0] - top_right[0]) +
+               (point[1] - top_right[1]) * (bot_right[1] - top_right[1]) < 0.0
+            {
+                return false;
+            }
+
+            if (point[0] - bot_right[0]) * (bot_left[0] - bot_right[0]) +
+               (point[1] - bot_right[1]) * (bot_left[1] - bot_right[1]) < 0.0
+            {
+                return false;
+            }
+
+            if (point[0] - bot_left[0]) * (top_left[0] - bot_left[0]) +
+               (point[1] - bot_left[1]) * (top_left[1] - bot_left[1]) < 0.0
+            {
+                return false;
+            }
+
+            true
+        }
+
+        match self {
+            &Shape::Text { ref matrix, .. } => {
+                test(matrix, point)
+            },
+
+            &Shape::Image { ref matrix, .. } => {
+                test(matrix, point)
+            },
+        }
+    }
+}
